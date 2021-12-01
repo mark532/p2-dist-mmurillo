4c904c8e598051522d6c2f54fd05d03d39bdcf9e
mark532 <mmurillo532@gmail.com>
2021-11-30 23:32:07 -0600

    c

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@ import './assets/css/normalize.css';
 import './assets/css/skeleton.css';
 
 const app = createApp(App);
-  
-app.config.globalProperties.url = 'https://p2-articulos-cientificos.netlify.app';
+
+app.config.globalProperties.url = 'https://affectionate-beaver-ad9e27.netlify.app';
 
 app.use(router).mount("#app");
